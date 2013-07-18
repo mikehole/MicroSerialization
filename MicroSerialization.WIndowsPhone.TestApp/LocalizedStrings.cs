f8cfbb6f1bd238619caa8a6e4fda8fab74885e09
mike@mikehole.com <mike@mikehole.com>
2013-07-18 17:38:03 +0100

    Added Test Projects

---
+++ MicroSerialization.WIndowsPhone.TestApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MicroSerialization.WIndowsPhone.TestApp.Resources;
+
+namespace MicroSerialization.WIndowsPhone.TestApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
